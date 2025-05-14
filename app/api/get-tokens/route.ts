f731d2d1f5ec5d7350d7583ea80cbc4464fc839d
IpDaniel <ip.d@northeastern.edu>
2025-05-13 20:51:50 -0400

    updated redirectUri

--- app/api/get-tokens/route.ts
+++ app/api/get-tokens/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const clientId = process.env.CONSTANT_CONTACT_CLIENT_ID;
-  const redirectUri = "https://your-vercel-app.vercel.app/api/callback"; // Update this
+  const redirectUri = "https://web-form-automation-ccapi.vercel.app/api/callback"; 
   
   const authUrl = `https://authz.constantcontact.com/oauth2/default/v1/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=contact_data`;
   
